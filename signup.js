8a25aea6ae6c2d69725926f301f3ec145df06f0d
argelalan <campos.alan@protonmail.com>
2024-04-16 14:36:02 -0700

    Updated signup.js

--- signup.js
+++ signup.js
@@ -27,3 +27,14 @@ console.log(createUser("user@example.com", "password456")); // User already exis
 console.log(signIn("user@example.com", "password123")); // Sign in successful.
 console.log(signIn("user@example.com", "wrongpassword")); // Invalid email or password.
 
+const express = require('express');
+const app = express();
+const port = 5001;
+
+app.get('/signup.html', (req, res) => {
+    res.sendFile('pages/signup', {root: __dirname});
+});
+
+app.listen(port, () => { 
+    console.log(`Now listening on port ${port}`); 
+});
