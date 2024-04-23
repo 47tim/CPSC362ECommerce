857fd67fed69e2668dfd215dec5b217327036fc8
Tim Hyde <hydet47@gmail.com>
2024-04-23 14:12:37 -0700

    css works

--- login.js
+++ login.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile('pages/login.html', {root: __dirname});
 });
@@ -9,4 +12,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => { 
     console.log(`Now listening on port ${port}`); 
 });
-  
