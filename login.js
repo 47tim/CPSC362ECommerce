ad3cf387310211cc2239fe0c0f96a52a883cfdf8
Tim Hyde <hydet47@gmail.com>
2024-04-25 13:43:33 -0700

    fix

--- login.js
+++ login.js
@@ -5,13 +5,17 @@ const port = 5001;
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile("pages/login.html", { root: __dirname });
+app.get('/', (req, res) => {
+  res.sendFile('pages/login.html', { root: __dirname });
 });
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
+
+/*
+
+Seperate file:
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("loginButton").addEventListener("click", handleLogin);
@@ -24,3 +28,5 @@ function handleLogin() {
   console.log(userName);
   console.log(password);
 }
+
+*/
