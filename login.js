a1f46c8eeff8af023fb62caa4816226b0a46e3b2
YoKarson <lantk991@gmail.com>
2024-04-25 13:30:49 -0700

    trying to add functinallity to log in button

--- login.js
+++ login.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5001;
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile('pages/login.html', {root: __dirname});
+app.get("/", (req, res) => {
+  res.sendFile("pages/login.html", { root: __dirname });
 });
 
-app.listen(port, () => { 
-    console.log(`Now listening on port ${port}`); 
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loginButton").addEventListener("click", handleLogin);
+});
+
+function handleLogin() {
+  var userName = document.getElementsByClassName("username").value;
+  var password = document.getElementsByClassName("password").value;
+
+  console.log(userName);
+  console.log(password);
+}
