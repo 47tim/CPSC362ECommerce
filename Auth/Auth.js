9b13bcbbf1a53e0561bc7ac89e7981e4aa04b31d
argelalan <campos.alan@protonmail.com>
2024-04-30 11:15:36 -0700

    Refactored register function.

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -1,26 +1,45 @@
 const User = require("../model/User")
+const bcrypt = require("bcryptjs")
 
 exports.register = async (req, res, next) => {
   const { username, password } = req.body
   if (password.length < 6) {
     return res.status(400).json({ message: "Password less than 6 characters" })
   }
-  try {
+  // try {
+  //   await User.create({
+  //     username,
+  //     password,
+  //   }).then(user =>
+  //     res.status(200).json({
+  //       message: "User successfully created",
+  //       user,
+  //     })
+  //   )
+  // } catch (err) {
+  //   res.status(401).json({
+  //     message: "User not successful created",
+  //     error: error.mesage,
+  //   })
+  // }
+  bcrypt.hash(password, 10).then(async (hash) => {
     await User.create({
       username,
-      password,
-    }).then(user =>
-      res.status(200).json({
-        message: "User successfully created",
-        user,
-      })
-    )
-  } catch (err) {
-    res.status(401).json({
-      message: "User not successful created",
-      error: error.mesage,
+      password: hash,
     })
-  }
+      .then((user) =>
+        res.status(200).json({
+          message: "User successfully created",
+           user,
+        })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          message: "User not successful created",
+          error: error.message,
+        })
+      );
+  });
 }
 
 exports.login = async (req, res, next) => {
