08ecd291b4234d6a859220494a98d11aa398f149
YoKarson <lantk991@gmail.com>
2024-05-07 00:46:22 -0700

    updated to handle balance correctly

--- pages/seasonalItems.js
+++ pages/seasonalItems.js
@@ -1,46 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var userBalance = parseInt(localStorage.getItem("userBalance")) || 0;
+var userBalance = parseInt(localStorage.getItem("userBalance")) || 0;
 
+function confirmPurchase(itemName, price) {
+  if (userBalance < price) {
+    alert("Insufficient funds!");
+    return;
+  }
+
+  var confirmPurchase = confirm(
+    "Are you sure you want to buy " + itemName + " for " + price + " Coins?"
+  );
+
+  if (confirmPurchase) {
+    userBalance -= price;
+
+    // Update user balance display
+    var coinBarElement = document.querySelector(".coinBar");
+    if (coinBarElement) {
+      coinBarElement.innerText = "Coins: " + userBalance;
+    } else {
+      console.error("Element with class 'coinBar' not found.");
+    }
+
+    purchasedItemName = itemName;
+    purchasedItemPrice = price;
+    purchaseDate = new Date();
+
+    // Save purchase details to local storage
+    localStorage.setItem("userBalance", userBalance);
+    localStorage.setItem("purchasedItemName", purchasedItemName);
+    localStorage.setItem("purchasedItemPrice", purchasedItemPrice);
+    localStorage.setItem("purchaseDate", purchaseDate);
+
+    alert("Purchase successful!");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   // Display user balance in coinBar
   var coinBarElement = document.querySelector(".coinBar");
   if (coinBarElement) {
     coinBarElement.innerText = "Coins: " + userBalance;
   } else {
     console.error("Element with class 'coinBar' not found.");
-  }
-
-  function confirmPurchase(itemName, price) {
-    if (userBalance < price) {
-      alert("Insufficient funds!");
-      return;
-    }
-
-    var confirmPurchase = confirm(
-      "Are you sure you want to buy " + itemName + " for " + price + " Coins?"
-    );
-
-    if (confirmPurchase) {
-      userBalance -= price;
-
-      // Update user balance display
-      var coinBarElement = document.querySelector(".coinBar");
-      if (coinBarElement) {
-        coinBarElement.innerText = "Coins: " + userBalance;
-      } else {
-        console.error("Element with class 'coinBar' not found.");
-      }
-
-      purchasedItemName = itemName;
-      purchasedItemPrice = price;
-      purchaseDate = new Date();
-
-      // Save purchase details to local storage
-      localStorage.setItem("userBalance", userBalance);
-      localStorage.setItem("purchasedItemName", purchasedItemName);
-      localStorage.setItem("purchasedItemPrice", purchasedItemPrice);
-      localStorage.setItem("purchaseDate", purchaseDate);
-
-      alert("Purchase successful!");
-    }
   }
 });
