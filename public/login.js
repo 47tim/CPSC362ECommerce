542b5897f1add212c077f8212cfd7bc3b48ea835
argelalan <campos.alan@protonmail.com>
2024-05-06 17:03:21 -0700

    Added compatibility with mocha and chai testing frameworks.

--- public/login.js
+++ public/login.js
@@ -1,9 +1,33 @@
 function handleLogin() {
-  var password = document.getElementById("password-input").value;
-  var userName = document.getElementById("username-input").value;
+  // var password = document.getElementById("password-input").value;
+  // var userName = document.getElementById("username-input").value;
 
-  console.log(userName);
-  console.log(password);
+  // console.log(userName);
+  // console.log(password);
 
-  window.location.href = "pages/index.html";
+  // window.location.href = "pages/index.html";
+  const form = document.querySelector('form')
+  const username = document.querySelector('#username')
+  const password = document.querySelector('#password')
+  const display = document.querySelector('.error')
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    display.textContent = ''
+    try {
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({ username: username.value, password: password.value }),
+        headers: { 'Content-Type': 'application/json' }
+        })
+      const data = await res.json()
+      if (res.status === 400 || res.status === 401) {
+        return display.textContent = `${data.message}. ${data.error ? data.error : ''}`
+      }
+      data.role === "admin" ? location.assign('/admin') : location.assign('/basic')
+    } catch (err) {
+        console.log(err.message)
+    }
+  })
+
+  window.location.href = "pages/index.ejs";
 }
