f7ae7a098864f29f903874047a0915727e9fd307
YoKarson <lantk991@gmail.com>
2024-05-06 23:20:25 -0700

    updating links

--- pages/settings.js
+++ pages/settings.js
@@ -3,6 +3,8 @@ var purchasedItemName = localStorage.getItem("purchasedItemName");
 var purchasedItemPrice = localStorage.getItem("purchasedItemPrice");
 var purchaseDate = localStorage.getItem("purchaseDate");
 
+console.log(purchasedItemName);
+
 document.getElementById("transactionDate").innerText = purchaseDate;
 document.getElementById("transactionItem").innerText = purchasedItemName;
 document.getElementById("transactionCost").innerText = purchasedItemPrice;
